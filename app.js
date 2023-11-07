01fbaa196460ab52cb37e044de13a6de61ba685f
your name <luonghuy148894@gmail.com>
2023-11-07 14:34:25 +0700

    setup and fake data

---
+++ app.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const port = 3004;
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side', app: 'Natours' });
+// });
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    result: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
