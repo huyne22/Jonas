eb2571abb4d89a2bda99e5aef80e7f9909e52ecb
your name <luonghuy148894@gmail.com>
2023-11-09 23:38:00 +0700

    57

--- app.js
+++ app.js
@@ -1,36 +1,53 @@
 const fs = require('fs');
 const express = require('express');
 const app = express();
+const port = 3004;
 app.use(express.json());
-// app.get('/', (req, res) => {
-//   res
-//     .status(200)
-//     .json({ message: 'Hello from the server side', app: 'Natours' });
-// });
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
-app.get('/api/v1/tours/:id', (req, res) => {
-  console.log(req.params)
-  const id = req.params.id * 1;
-  const tour = tours.find(el => el.id === req.params)
-if(!tour){
+const getAllTours = (req, res) => {
   res.status(200).json({
-    status: 'fail',
-    message : 'err'
+    status: 'success',
+    result : tours.length,
+    data: {
+      tours
+    },
   });
 }
-
-
-
+const getTour =  (req, res) => {
+  console.log(req.params)
+  const id = req.params.id * 1;
+  const tour = tours.find(el => el.id === id)
+  if(!tour){
+    res.status(200).json({
+      status: 'fail',
+      message : 'err'
+    });
+  }
   res.status(200).json({
     status: 'success',
     data: {
       tour,
     },
   });
-});
-app.post('/api/v1/tours', (req, res) => {
+}
+const updateTour = (req,res) => {
+  const id = req.params.id * 1;
+  if(id > tours.length){
+   return res.status(404).json({
+      status: 'fail',
+      message : 'Tour not found'
+    });
+  }
+  res.status(200).json({
+    status : 'success',
+    data: {
+      tour : '<Update data ...>'
+    }
+  })
+}
+const createTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   console.log(newId)
   const newTour = Object.assign({ id: newId }, req.body);
@@ -47,8 +64,33 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
-});
-const port = 3004;
+}
+const deleteTour =  (req,res) => {
+  const id = req.params.id * 1;
+  if(id > tours.length){
+   return res.status(404).json({
+      status: 'fail',
+      message : 'Tour not found'
+    });
+  }
+  res.status(204).json({
+    status : 'success',
+    data: null
+  })
+}
+// app.get('/api/v1/tours', getAllTours);
+// app.get('/api/v1/tours/:id', getTour);
+// app.post('/api/v1/tours', createTour);
+// app.patch('/api/v1/tours/:id', updateTour)
+// app.delete('/api/v1/tours/:id',deleteTour)
+
+app.route('/api/v1/tours')
+    .get(getAllTours)
+    .post(createTour)
+app.route('/api/v1/tours/:id')
+    .get(getTour)
+    .patch(updateTour)
+    .delete(deleteTour)
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
