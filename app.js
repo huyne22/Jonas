92b1148029bdfd13d89b105d22e151aae0d1526b
your name <luonghuy148894@gmail.com>
2023-12-03 18:51:04 +0700

    setup db mongo

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const fs = require('fs');
 const express = require('express');
 const morgan =require('morgan');
-const app = express();
+
 const userRouter = require('./routes/userRouters')
 const tourRouter = require('./routes/tourRouters');
 
+const app = express();
+
 //1, Middleware
-app.use(express.json());
+app.use(express.json())
+
 app.use(express.static(`${__dirname}/public`));
-if(process.env.NODE_ENV === 'development'){
+if(process.env.NODE_ENV === "development"){
   app.use(morgan('dev'));
 }
+
 app.use((req,res,next) => {
   console.log("Hello word!!!");
   next();
--- server.js
+++ server.js
@@ -1,8 +1,19 @@
+const mongoose = require('mongoose');
+
 const dotenv = require('dotenv');
-dotenv.config({ path: '/.env' })
+dotenv.config({ path: './.env' })
+
+const DB = process.env.MONGO.replace('<password>', process.env.PASS_MONGO);
+mongoose.connect(DB, {
+  useNewUrlParser:true,
+  useCreateIndex:true,
+  useFindAndModify:false,
+  useUnifiedTopology:true
+}).then(con => {
+  console.log("DB connections success!")
+})
+
 const app = require('./app');
-console.log(process.env)
-console.log(process.env.NODE_ENV)
 const port = process.env.PORT || 3004;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
